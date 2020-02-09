1a8966b483000c2038055e49d93a8f15a51421b1
GuliusJ <GuliusJ@users.noreply.github.com>
2020-02-09 04:57:31 +0100

    db-reset: suppressing 'zurich'

--- Tools/BullingerDB.py
+++ Tools/BullingerDB.py
@@ -468,10 +468,6 @@ class BullingerDB:
         return data_overview, data_percentages, plot_url, num_of_cards
 
     @staticmethod
-    def get_progress_data():
-        pass
-
-    @staticmethod
     def normalize_str_input(value):
         if not isinstance(value, str): return None
         elif not value.strip(): return None
